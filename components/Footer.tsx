0f0a78713bf3f0238fecc14aaa23b47950a222db
Teresa <teresatoledolara@gmail.com>
2024-06-12 11:07:56 +0200

    Add portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { Separator } from './ui/separator';
-import { Github, Linkedin } from 'lucide-react';
+import { Briefcase, Github, Linkedin } from 'lucide-react';
 import logo from '../public/logo.png';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -29,7 +29,7 @@ function Footer() {
         <footer className="mt-4">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="flex items-center justify-between flex-col sm:flex-row">
-                    <div className="flex gap-10 items-center justify-center pl-4">
+                    <div className="flex gap-5 sm:gap-10 items-center justify-center pl-4">
                         <Image
                             src={logo}
                             alt="Logo de calcetopía"
@@ -50,8 +50,15 @@ function Footer() {
                         >
                             <Github />
                         </a>
+                        <a
+                            href="https://teresatoledo.vercel.app/"
+                            target="_blank"
+                            className="text-sm text-gray-500  dark:text-gray-400"
+                        >
+                            <Briefcase/>
+                        </a>
                     </div>
-                    <ul className="flex flex-col sm:flex-row flex-wrap items-center mb-6 mt-4 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <ul className="flex flex-col sm:flex-row flex-wrap items-center mb-6 mt-4 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-40">
                         {dataFooter.map((data) => (
                             <li key={data.id}>
                                 <Link
